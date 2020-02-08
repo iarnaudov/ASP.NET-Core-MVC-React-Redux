b4197f14e0f9ec0e520f05cb8a5ea378eede67d1
iarnaudov91 <iarnaudov91@gmail.com>
2020-02-08 21:08:05 +0200

    Project Update

---
+++ ASP.NET.MVC.Core/Data/AppCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASP.NET.MVC.Core.Models;
+
+namespace ASP.NET.MVC.Core.Data
+{
+    public class AppCoreContext : DbContext
+    {
+        public AppCoreContext (DbContextOptions<AppCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASP.NET.MVC.Core.Models.Book> Books { get; set; }
+    }
+}
---
+++ ASP.NET.MVC.Core/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET.MVC.Core.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
